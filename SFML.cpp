a231ae9be3b13c98a423826a21e805a8c9acc5e8
Eugene Mazin <eugene.mazin@gmail.com>
2020-05-05 09:40:48 -0700

    28 April

--- SFML.cpp
+++ SFML.cpp
@@ -1,25 +1,57 @@
 #include <SFML/Graphics.hpp>
 
-void movement(sf::Transformable& shape, sf::Event event)
+class Mario
 {
-    if(event.key.code == sf::Keyboard::Left)
-        shape.move(-1, 0);
-    else if (event.key.code == sf::Keyboard::Right)
-        shape.move(1, 0);
-    else if (event.key.code == sf::Keyboard::Up)
-        shape.move(0, -1);
-    else if (event.key.code == sf::Keyboard::Down)
-        shape.move(0, 1);
-}
+public:
+    Mario()
+    {
+        groundLevel = 300;
+        image.loadFromFile("mario.png");
+        mario = sf::Sprite(image, sf::IntRect(0, 98, 80, 80));
+        mario.setPosition(0, groundLevel);
+    }
+
+    void draw(sf::RenderWindow& window)
+    {
+        if (clock.getElapsedTime() > sf::seconds(0.1f))
+        {
+            clock.restart();
+            mario.move(speed);
+            auto pos = mario.getPosition().y - groundLevel;
+            speed -= {0, pos / 10 };
+        }
+        window.draw(mario);
+    }
+
+    void move(sf::Event event)
+    {
+        if (event.key.code == sf::Keyboard::Left)
+            speed -= { 1, 0 };
+        else if (event.key.code == sf::Keyboard::Right)
+            speed += { 1, 0 };
+        else if (event.key.code == sf::Keyboard::Up)
+            speed += { 0, -50 };
+        else if (event.key.code == sf::Keyboard::Down)
+            mario.move(0, 1);
+    }
+
+private:
+    sf::Texture image;
+    sf::Sprite mario;
+    sf::Vector2f speed;
+    sf::Clock clock;
+    int groundLevel;
+};
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(600, 400), "Mario!");
-    sf::CircleShape shape(10.f);
-    sf::Texture image;
-    image.loadFromFile("mario.png");
-    sf::Sprite mario(image, sf::IntRect(100, 100, 120, 120));
-    shape.setFillColor(sf::Color::Green);
+    sf::CircleShape virus(10.f);
+    virus.setPosition(580, 200);
+    Mario mario;
+    virus.setFillColor(sf::Color::Green);
+
+    sf::Clock clock;
 
     while (window.isOpen())
     {
@@ -30,11 +62,20 @@ int main()
                 window.close();
 
             if (event.type == sf::Event::KeyPressed)
-                movement(mario, event);
+                mario.move(event);
+        }
+
+        if (clock.getElapsedTime() > sf::seconds(0.1f))
+        {
+            clock.restart();
+            if (virus.getPosition().x < 0)
+                virus.setPosition(580, 200);
+            virus.move(-1, 0);
         }
 
         window.clear();
-        window.draw(mario);
+        window.draw(virus);
+        mario.draw(window);
         window.display();
     }
 
